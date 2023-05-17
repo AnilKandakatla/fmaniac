10b2553ec1844fe39d663b6ff79987e48a1cfaed
AnilKandakatla <anilkandakatla98@gmail.com>
2023-05-17 21:53:22 +1000

    chnges

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from 'react'
 //import Banner from '../components/Banner'
-import LandingPage from '../components/LandingPage'
+//import LandingPage from '../components/LandingPage'
 import Products from '../components/Products'
 import { useLoaderData } from 'react-router-dom'
+import bgVideo from '../video/bgVideo.mp4'
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-auto overflow-x-hidden">
+        <div className="w-screen relative">
+        <video src={bgVideo} autoPlay muted loop class="video-bg" />
+        </div>
+    </div>
+  )
+}
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -12,9 +23,8 @@ const Home = () => {
   },[data])
   return (
     <div>
-      {/*<Banner />*/}
       <LandingPage />
-      <Products products={products} />
+      <Products products={products} />  
     </div>
   )
 }
